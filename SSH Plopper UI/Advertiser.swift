fe26bf9c948063d336467fc1def37cae1a5b7ba3
Anoop Gopalakrishnan <agopalakrishnan@pivotal.io>
2016-05-06 13:06:25 -0700

    Add ssh key to ssh-agent working
    Signed-off-by: Matthew Boedicker <mboedicker@pivotal.io>

--- SSH Plopper UI/Advertiser.swift
+++ SSH Plopper UI/Advertiser.swift
@@ -1,33 +1,64 @@
 import Cocoa
 import MultipeerConnectivity
 
-class Advertiser: NSObject, MCNearbyServiceAdvertiserDelegate {
+class Advertiser: NSObject, MCNearbyServiceAdvertiserDelegate, MCSessionDelegate {
 
     let advertiser: MCNearbyServiceAdvertiser
+    let localPeerId : MCPeerID
+    let session : MCSession
 
     override init() {
-        let peerId = MCPeerID(displayName: "minna")
-
+        localPeerId = MCPeerID(displayName: "balboa")
         let serviceType = "ssh-key-service"
-
-        advertiser = MCNearbyServiceAdvertiser(peer: peerId, discoveryInfo: nil, serviceType: serviceType)
-
+        advertiser = MCNearbyServiceAdvertiser(peer: localPeerId, discoveryInfo: nil, serviceType: serviceType)
+        session = MCSession(peer: localPeerId, securityIdentity: nil, encryptionPreference: .Required)
         super.init()
     }
 
     func advertise() {
         advertiser.delegate = self
         advertiser.startAdvertisingPeer()
+        session.delegate = self;
 
         print("Started to listen")
     }
 
     func advertiser(advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: NSData?, invitationHandler: (Bool, MCSession) -> Void) {
         print("Received invitation from ", peerID)
+        let acceptedInvitation = true
+        session.delegate = self;
+        invitationHandler(acceptedInvitation, session)
     }
 
     func advertiser(advertiser: MCNearbyServiceAdvertiser, didNotStartAdvertisingPeer error: NSError) {
         print("Error:", error)
+    }
+    
+    func session(session: MCSession, didReceiveData data: NSData, fromPeer peerID: MCPeerID) {
+        print("Received data :" + data.description)
+        let sshHelper = SSHHelper()
+        sshHelper.addKey(data)
+    }
+    
+    func session(session: MCSession, didReceiveCertificate certificate: [AnyObject]?, fromPeer peerID: MCPeerID, certificateHandler: (Bool) -> Void) {
+        print("Did receive certificate")
+        certificateHandler(true)
+    }
+    
+    func session(session: MCSession, didReceiveStream stream: NSInputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        print("did receive stream" + streamName)
+    }
+    
+    func session(session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, withProgress progress: NSProgress) {
+        print("did start receiving resource " + resourceName)
+    }
+    
+    func session(session: MCSession, peer peerID: MCPeerID, didChangeState state: MCSessionState) {
+        print("change state" + peerID.displayName, state.rawValue)
+    }
+    
+    func session(session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, atURL localURL: NSURL, withError error: NSError?) {
+        print("did finish receiving resource " + resourceName)
     }
     
 }
---
+++ SSH Plopper UI/CommandRunner.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+class SSHHelper {
+    func addKey(data: NSData) -> Int {
+        let task = NSTask()
+        task.launchPath = "/usr/bin/ssh-add"
+        task.arguments = ["-t", "1m", "-"]
+        let pipe = NSPipe()
+        pipe.fileHandleForWriting.writeData(data)
+        pipe.fileHandleForWriting.closeFile()
+        task.standardInput = pipe
+        task.launch()
+        task.waitUntilExit()
+        return Int(task.terminationStatus)
+    }
+    
+    
+}
